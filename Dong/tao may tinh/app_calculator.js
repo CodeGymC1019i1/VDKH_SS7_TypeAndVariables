fb533f733e9cc4783d55431e3b66ed76edac5c95
dong94vk <quangdong.071094@gmail.com>
2019-11-05 21:23:17 +0700

    update bai tap 21:23

--- Dong/tao may tinh/app_calculator.js
+++ Dong/tao may tinh/app_calculator.js
@@ -1,3 +1,8 @@
-function calc() {
+function insertValueOfDisplay(x) {
+    eval('display').value += x;
+}
 
+function calc() {
+    let display = document.getElementById('display');
+    display.value = eval(display.value).toFixed(3);
 }
